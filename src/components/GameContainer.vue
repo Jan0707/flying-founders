b5d1426777a7542a4d3d0e287ac7eaa709cb23ba
david_lotum <koschitzki@lotum.de>
2023-11-07 18:31:26 +0100

    feat: create dumb placeholder logic for level transitions

--- src/components/GameContainer.vue
+++ src/components/GameContainer.vue
@@ -4,17 +4,18 @@ import LevelInfo from "./LevelInfo.vue";
 import MenuItems from "./MenuItems.vue";
 import { ref, watch } from "vue";
 import { levelState } from "../game/levelState.ts";
+import LevelFinishedDialog from "./dialogs/LevelFinishedDialog.vue";
 
 const levelKeys = ref(1);
 const levelName = ref("1");
+
+const isLevelFinished = ref(false);
 
 watch(
   () => levelState.targets,
   () => {
     if (levelState.targets === 0) {
-      levelName.value = "2";
-      levelKeys.value += 1;
-      console.log("levelName: ", levelName.value);
+      isLevelFinished.value = true;
     }
   },
 );
@@ -22,14 +23,31 @@ watch(
 function reset() {
   levelKeys.value += 1;
 }
+
+function getNextLevel() {
+  isLevelFinished.value = false;
+  if (levelName.value === "1") {
+    levelName.value = "2";
+  } else {
+    levelName.value = "1";
+  }
+
+  levelKeys.value += 1;
+}
 </script>
 
 <template>
-  <div class="top-bar">
+  <div v-if="!isLevelFinished" class="top-bar">
     <LevelInfo />
     <MenuItems @reset="reset" />
   </div>
   <Level :key="levelKeys" :levelName="levelName" />
+  <LevelFinishedDialog
+    v-if="isLevelFinished"
+    class="dialog"
+    :levelName="levelName"
+    @continue="getNextLevel()"
+  />
 </template>
 
 <style scoped>
@@ -37,5 +55,11 @@ function reset() {
   display: flex;
   justify-content: space-between;
   align-items: center;
+}
+
+.dialog {
+  position: absolute;
+  top: 35%;
+  left: 25%;
 }
 </style>
