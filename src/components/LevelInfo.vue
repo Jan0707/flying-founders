5f49d70cd3a329dc06f10ca6477960810766db94
david_lotum <koschitzki@lotum.de>
2023-11-07 16:41:15 +0100

    feat: create components for general level information

---
+++ src/components/LevelInfo.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { levelState } from "../game/levelState.ts";
+</script>
+
+<template>
+  <div>
+    <p class="shot-counter">Shots fired: {{ levelState.shots }}</p>
+    <p class="points-counter">Points: {{ levelState.points }}</p>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/MenuItems.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { levelState } from "../game/levelState.ts";
+
+const emit = defineEmits(["reset"]);
+</script>
+
+<template>
+  <img
+    src="../assets/replay-icon.png"
+    class="replay-button"
+    alt="Replay button"
+    @click="emit('reset')"
+  />
+</template>
+
+<style scoped>
+.replay-button {
+  width: 50px;
+  cursor: pointer;
+}
+</style>
