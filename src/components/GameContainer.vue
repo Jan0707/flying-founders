ccbb92059d553ec427872dd426da8584c3eca9e2
david_lotum <koschitzki@lotum.de>
2023-11-07 16:41:36 +0100

    refac: rename HelloWorld to GameContainer

---
+++ src/components/GameContainer.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import Level from "./Level.vue";
+import LevelInfo from "./LevelInfo.vue";
+import MenuItems from "./MenuItems.vue";
+import { ref } from "vue";
+
+const levelKeys = ref(1);
+
+function reset() {
+  levelKeys.value += 1;
+}
+</script>
+
+<template>
+  <div class="top-bar">
+    <LevelInfo />
+    <MenuItems @reset="reset" />
+  </div>
+  <Level :key="levelKeys" levelName="1" />
+</template>
+
+<style scoped>
+.top-bar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
