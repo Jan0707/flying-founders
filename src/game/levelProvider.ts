025a32082cd30639549b06fdfaef026f9cd8c1e3
Sven Szota <svszota@gmail.com>
2023-12-05 12:30:56 +0100

    Level 4 Build. Construction breaks instantly.

--- src/game/levelProvider.ts
+++ src/game/levelProvider.ts
@@ -1,6 +1,7 @@
 import { getLevel as getLevel1 } from "./levels/level1.ts";
 import { getLevel as getLevel2 } from "./levels/level2.ts";
 import { getLevel as getLevel3 } from "./levels/level3.ts";
+import { getLevel as getLevel4 } from "./levels/level4.ts";
 import { LevelCreatorFunction } from "./Level.ts";
 
 class LevelProvider {
@@ -11,6 +12,7 @@ class LevelProvider {
     this.levels.set("1", getLevel1);
     this.levels.set("2", getLevel2);
     this.levels.set("3", getLevel3);
+    this.levels.set("4", getLevel4);
   }
 
   getLevelByName(name: string): LevelCreatorFunction | undefined {
